546f08d2e82444f20568ac66c0fe4d498c37cf9c
Michelle Kaye Lagunsad <mlagunsad@gmail.com>
2023-05-06 14:40:15 -0700

    Fix editor role id

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         {/* public routes */}
-            <Route element={<LayoutMinimal />}>
+        <Route element={<LayoutMinimal />}>
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Register />} />
             <Route path="linkpage" element={<LinkPage />} />
